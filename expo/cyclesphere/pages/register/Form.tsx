e43f0754244862d48a2ad463064462615796ff6f
asdtae <112775132+asdtae@users.noreply.github.com>
2023-11-18 21:31:22 +0200

    register page app

--- expo/cyclesphere/pages/register/Form.tsx
+++ expo/cyclesphere/pages/register/Form.tsx
@@ -1,47 +1,67 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import ReCAPTCHA from 'react-google-recaptcha';
-import Config from 'react-native-config';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Image,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as SecureStore from 'expo-secure-store';
+import zxcvbn from 'zxcvbn'; // For password strength checking
 
-function SettingsScreen() {
+function RegisterScreen() {
   const [inputs, setInputs] = useState({
     username: '',
     email: '',
     password: '',
-    phoneNumber: '',
+    confirmPassword: '',
   });
 
-  const recaptchaRef = useRef<any>(null);
+  const [profilePic, setProfilePic] = useState(null);
 
-  const handleInputChange = (key: string, value: string) => {
+  const handleInputChange = (key: any, value: any) => {
     setInputs({
       ...inputs,
       [key]: value,
     });
   };
 
-  const handleSubmit = () => {
-    if (recaptchaRef.current) {
-      recaptchaRef.current.verify();
-    }
+  const handleProfilePicUpload = async () => {
+    // Implement image uploading logic using ImagePicker
+    // Set the selected image to 'profilePic'
   };
 
-  const onVerify = (token: string) => {
-    console.log('reCAPTCHA token:', token);
-    // Here, you can include your form submission logic using the token.
-    // For example, send the form data along with the token to a server.
-    console.log('Form submitted:', inputs);
+  const handleSubmit = () => {
+    if (inputs.password !== inputs.confirmPassword) {
+      alert("Passwords don't match!");
+      return;
+    }
+
+    // Check password strength using zxcvbn library
+    const passwordStrength = zxcvbn(inputs.password);
+    if (passwordStrength.score < 3) {
+      alert(
+        'Please create a stronger password. Include numbers, special characters, and both uppercase and lowercase letters.'
+      );
+      return;
+    }
+
+    // Save user data and profile picture to database or storage
+    // Redirect to the home screen or perform further actions
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Username</Text>
-      <TextInput
-        style={styles.input}
-        value={inputs.username}
-        onChangeText={(text) => handleInputChange('username', text)}
-        placeholder="Enter username"
-      />
+      <View style={styles.formContainer}>
+        {/* Profile picture upload */}
+        <TouchableOpacity style={styles.profilePicButton} onPress={handleProfilePicUpload}>
+          <Text style={styles.profilePicButtonText}>Upload Profile Picture</Text>
+        </TouchableOpacity>
+        {profilePic && <Image source={{ uri: profilePic }} style={styles.profilePic} />}
+
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          value={inputs.username}
+          onChangeText={(text) => handleInputChange('username', text)}
+          placeholder="Enter username"
+        />
 
       <Text style={styles.label}>Email</Text>
       <TextInput
@@ -61,23 +81,17 @@ function SettingsScreen() {
         secureTextEntry
       />
 
-      <Text style={styles.label}>Phone Number</Text>
+      <Text style={styles.label}>Confirm Password</Text>
       <TextInput
         style={styles.input}
-        value={inputs.phoneNumber}
-        onChangeText={(text) => handleInputChange('phoneNumber', text)}
-        placeholder="Enter phone number"
-        keyboardType="phone-pad"
+        value={inputs.confirmPassword}
+        onChangeText={(text) => handleInputChange('confirmPassword', text)}
+        placeholder="Confirm password"
+        secureTextEntry
       />
-
-      <ReCAPTCHA
-        ref={recaptchaRef}
-        sitekey={Config.REACT_APP_SITE_KEY}
-        onVerify={onVerify}
-        //style={styles.recaptcha} // Style the reCAPTCHA component as needed
-      />
-
-      <Button title="Submit" onPress={handleSubmit} />
+        
+        <Button title="Register" onPress={handleSubmit} />
+      </View>
     </View>
   );
 }
@@ -85,11 +99,22 @@ function SettingsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
     padding: 20,
   },
+  formContainer: {
+    width: '80%',
+    backgroundColor: 'purple', // Blue box background color
+    padding: 20,
+    borderRadius: 15, // Rounded corners
+  },
+
   label: {
     fontWeight: 'bold',
     marginBottom: 5,
+    color: 'white', // Label text color
   },
   input: {
     borderWidth: 1,
@@ -97,7 +122,29 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 15,
+    backgroundColor: 'white', // Input background color
+  },
+  profilePicButton: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  profilePicButtonText: {
+    color: 'blue',
+  },
+  profilePic: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    borderRadius: 50,
+    alignSelf: 'center',
+    marginBottom: 20,
   },
 });
 
-export default SettingsScreen;
+export default RegisterScreen;
